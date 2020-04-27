4586cb5d8d7a3309604ae74f4b3a47a3be9172e9
lizza <liutao_glory@126.com>
2020-04-27 21:47:45 +0800

    lizza commit.

--- jvm_01_classload/src/main/java/com/lizza/ClassLoad_05.java
+++ jvm_01_classload/src/main/java/com/lizza/ClassLoad_05.java
@@ -1,20 +1,29 @@
 package com.lizza;
 
+import java.util.Random;
+
 /**
- * 1. 当一个接口初始化时, 并不要求其父接口都完成了初始化
+ * 1. 初始化一个类（接口）不会初始化其父接口
  * 2. 只有在真正使用到父接口时(比如引用了父接口中定义的常量), 才会完成初始化
+ * 3. 当一个类被初始化的时候, 它所实现的接口是不会被初始化的; 但是接口会被加载
+ * 4. 当一个类的变量为常量时, 使用该常量, 不会导致该类被加载, 更不会被初始化
  */
 public class ClassLoad_05 {
 
     public static void main(String[] args){
-        System.out.println(Child_05.b);
+        System.out.println(Child_05.a);
     }
 }
 
 interface Parent_05 {
-    int a = 5;
+    int a = new Random().nextInt(10);
+    public static Thread thread = new Thread(){
+        {
+            System.out.println("Parent_05 inited!");
+        }
+    };
 }
 
-interface Child_05 extends Parent_05 {
-    int b = 6;
+class Child_05 implements Parent_05 {
+    public static int b = 6;
 }
