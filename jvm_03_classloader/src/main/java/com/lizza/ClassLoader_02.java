ca22fe010c3b9b9ba1b61fdde35b94be4faf1fbb
lizza <liutao_glory@126.com>
2020-05-05 23:11:53 +0800

    lizza commit.

---
+++ jvm_03_classloader/src/main/java/com/lizza/ClassLoader_02.java
@@ -0,0 +1,24 @@
+package com.lizza;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+/**
+ * @Desc:
+ * @author: lizza1643@gmail.com
+ * @date: 2020-05-05
+ */
+public class ClassLoader_02 {
+
+    public static void main(String[] args) throws IOException {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        String url = "com/lizza/ClassLoader_01.class";
+        Enumeration<URL> resources = loader.getResources(url);
+        while (resources.hasMoreElements()) {
+            URL element = resources.nextElement();
+            System.out.println(element);
+        }
+
+    }
+}
