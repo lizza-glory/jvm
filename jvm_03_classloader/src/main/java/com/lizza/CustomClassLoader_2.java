9a86e83f9d41e898994c95d561543ea71e69d2d5
lizza <liutao_glory@126.com>
2020-05-09 10:25:09 +0800

    lizza commit.

--- jvm_03_classloader/src/main/java/com/lizza/CustomClassLoader_2.java
+++ jvm_03_classloader/src/main/java/com/lizza/CustomClassLoader_2.java
@@ -4,9 +4,18 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 /**
  * 修改: 从指定路径加载.class文件
+ * 1. 第一次执行(未删除CLASSPATH下的ClassLoader_01.class文件), 类加载器是系统类加载器
+ * 2. 删除CLASSPATH下的ClassLoader_01.class文件后再次执行, 类加载器是CustomClassLoader_2
+ * 原因:
+ * 1. 未删除CLASSPATH下的ClassLoader_01.class文件, 根据双亲委派原则, 类加载器会去委托父类加载器去加载ClassLoader_01类
+ *    而自定义类加载器的父类加载器是AppClassLoader(系统类加载器), AppClassLoader会在CLASSPATH下寻找并加载ClassLoader_01类
+ * 2. 删除了ClassLoader_01类后, AppClassLoader在CLASSPATH下寻找加载ClassLoader_01类, 加载失败后,
+ *    返回给自定义的类加载器CustomClassLoader_2进行类加载
+ * 注意:
  */
 public class CustomClassLoader_2 extends ClassLoader {
 
