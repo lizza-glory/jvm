44d6eed1108064a11be81bd47c7c3fef4b28d835
lizza <liutao_glory@126.com>
2020-05-28 20:28:06 +0800

    lizza commit.

--- jvm_03_classloader/src/main/java/com/lizza/ClassLoader_12.java
+++ jvm_03_classloader/src/main/java/com/lizza/ClassLoader_12.java
@@ -12,10 +12,17 @@ import java.sql.DriverManager;
  *       加载器可以打破双亲委派机制的限制
  *    2. Class.forName()方法在加载类时, 默认使用的是类的调用者的类加载器, 默认类加载器是
  *       AppClassLoader, 所以可以加载CLASSPATH下的jar
+ *    3. Class.forName(String className)默认会立即初始化所加载的类, 所以com.mysql.jdbc.Driver
+ *       会立即被初始化
+ *    4. com.mysql.jdbc.Driver符合Java SPI的规范, 所以类路径下/META-INF/services/java.sql.Driver
+ *       文件中定义的实现类默认都会被加载, 而且即使不写Class.forName("com.mysql.jdbc.Driver")也可以加载
+ *       com.mysql.jdbc.Driver
  * 2. 类的连接
  * 3. 类的初始化
  *    1. 当调用DriverManager.getConnection()方法时, 会导致类的初始化(原因: 调用类的静态
  *       方法, 属于对类的主动使用, 会导致类的初始化)
+ *    2. isDriverAllowed(Driver driver, ClassLoader classLoader)方法, 调用了Class.forName
+ *       来判断加载了Driver类的命名空间是否相同, 即判断是否是加载了调用类的类加载器加载的Driver
  */
 public class ClassLoader_12 {
 
@@ -23,7 +30,7 @@ public class ClassLoader_12 {
         // 使用指定的类加载器(ExtClassLoader)加载MySQL驱动时, 会抛出ClassNotFoundException
         // 原因: ExtClassLoader无法加载CLASSPATH下的jar
 //        Class.forName("com.mysql.jdbc.Driver", false, ClassLoader_12.class.getClassLoader().getParent());
-        Class.forName("com.mysql.jdbc.Driver");
+//        Class.forName("com.mysql.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
     }
 }
