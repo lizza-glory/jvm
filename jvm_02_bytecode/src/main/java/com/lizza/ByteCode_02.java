b79601198b6b8be23fc59948797a06ef81c22623
lizza <liutao_glory@126.com>
2020-06-07 21:43:19 +0800

    lizza commit.

--- jvm_02_bytecode/src/main/java/com/lizza/ByteCode_02.java
+++ jvm_02_bytecode/src/main/java/com/lizza/ByteCode_02.java
@@ -1,10 +1,23 @@
 package com.lizza;
 
 /**
- * @Desc:
- * @author: lizza1643@gmail.com
- * @date: 2020-06-07
+ * 分析字节码结构
  */
 public class ByteCode_02 {
 
+    String s = "hello";
+
+    private int x = 10;
+
+    public static int y = 20;
+
+    public static void main(String[] args){
+        ByteCode_02 byteCode_02 = new ByteCode_02();
+        byteCode_02.setX(8);
+        y = 28;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
 }
