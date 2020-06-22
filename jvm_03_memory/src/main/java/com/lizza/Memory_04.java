3e7e986c75037f762c0cc91b8a83effe1263a0c2
lizza <liutao_glory@126.com>
2020-06-22 10:04:45 +0800

    lizza commit.

--- jvm_03_memory/src/main/java/com/lizza/Memory_04.java
+++ jvm_03_memory/src/main/java/com/lizza/Memory_04.java
@@ -1,9 +1,25 @@
 package com.lizza;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+
 /**
- * 模拟方法区产生内存溢出
- *
+ * 模拟方法区(元空间, Meta Space)产生内存溢出
+ * -XX:MaxMetaspaceSize=5m: 设置元空间大小为固定大小, 5m
  */
 public class Memory_04 {
 
+    public static void main(String[] args) throws InterruptedException {
+        for (;;) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(Memory_04.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback((MethodInterceptor)(object, method, param, proxy) ->
+                proxy.invokeSuper(object, param)
+            );
+            Thread.sleep(1000);
+            Object o = enhancer.create();
+            System.out.println(o.getClass());
+        }
+    }
 }
