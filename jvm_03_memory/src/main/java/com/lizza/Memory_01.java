bb99e7521109cd31a2dec82a88887a691df60b05
lizza <liutao_glory@126.com>
2020-07-23 09:34:23 +0800

    lizza commit.

--- jvm_03_memory/src/main/java/com/lizza/Memory_01.java
+++ jvm_03_memory/src/main/java/com/lizza/Memory_01.java
@@ -7,6 +7,8 @@ import java.util.List;
  * -Xms2m: 最小堆内存2m
  * -Xmx2m: 最大堆内存2m
  * -XX:+HeapDumpOnOutOfMemoryError: 发生OOM时储存错误文件
+ * -XX:HeapDumpPath=/Users/lizza/Desktop/: 发生OOM时, dump文件存储位置
+ * -Xloggc:/Users/lizza/Desktop/gc.log: 发生GC时, gc文件的存储位置
  */
 public class Memory_01 {
 
@@ -14,8 +16,8 @@ public class Memory_01 {
         List<Object> list = new ArrayList<>();
         for(;;) {
             list.add(new Object());
-            System.gc();
-            Thread.sleep(1000);
+//            System.gc();
+//            Thread.sleep(1000);
         }
     }
 }
