1f9b75b981da1b8169ab44c9034722b47782ffff
lizza <liutao_glory@126.com>
2020-11-06 10:48:59 +0800

    lizza commit.

---
+++ jvm_04_gc/src/main/java/com/lizza/gc_02_cms/CMS_02.java
@@ -0,0 +1,24 @@
+package com.lizza.gc_02_cms;
+
+/**
+ * CMS调优实践
+ * 模拟频繁GC
+ * jvm参数: -Xms20m -Xmx20m -Xmn6m -XX:+PrintGCDetails -XX:+UseParNewGC -XX:+UseConcMarkSweepGC
+ */
+public class CMS_02 {
+
+    public static void m() {
+        int size = 1024 * 1024;
+        byte[] allocation1 = new byte[4 * size];
+        System.out.println("-------------------");
+        byte[] allocation2 = new byte[4 * size];
+        System.out.println("*******************");
+    }
+
+    public static void main(String[] args){
+    	while (true) {
+    	    m();
+    	    try { Thread.sleep(1500); } catch (Exception e) {}
+        }
+    }
+}
